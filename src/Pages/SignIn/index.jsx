2ee9b5345e4dedffb2f785b7d5777bf02377b766
Juan Carlos Gomez <juan.gomez@synergias.co>
2024-08-01 17:05:52 -0500

    Feature/1 Inclusion de mensajes

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,21 +1,19 @@
-import { useContext, useState, useRef, useEffect } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useContext, useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import { ShoppingCartContext } from "../../Context";
 import Layout from "../../Components/Layout";
 import { signInWs } from "../../services/AuthService";
-import "../../utils/css/animations.css";
+import MessageContainer from "../../Components/Message/MessageContainer";
+import { messages } from "../../consts/Utilities";
 
 function SignIn() {
   const context = useContext(ShoppingCartContext);
   const [view, setView] = useState("user-info");
-
   const form = useRef(null);
   const navigate = useNavigate();
 
-  // Account
   const account = localStorage.getItem("account");
   const parsedAccount = JSON.parse(account);
-  // Has an account
   const noAccountInLocalStorage = parsedAccount
     ? Object.keys(parsedAccount).length === 0
     : true;
@@ -23,26 +21,19 @@ function SignIn() {
     ? Object.keys(context.account).length === 0
     : true;
   const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
-  //const [showMessage, setShowMessage] = useState(false);
-  //const [isExiting, setIsExiting] = useState(false);
 
-  // useEffect(() => {
-  //   if (context.message) {
-  //     setShowMessage(true);
-  //     context.setMessage(context.message);
-  //     setTimeout(() => {
-  //       setIsExiting(true);
-  //       setTimeout(() => {
-  //         setShowMessage(false);
-  //         setIsExiting(false);
-  //         context.setMessage("");
-  //       }, 1000);
-  //     }, 4000);
-  //   }
-  // }, [context.message]);
+  const messageContainerRef = useRef(null);
+  const showMessage = (type, text) => {
+    if (messageContainerRef.current) {
+      messageContainerRef.current.addMessage(type, text);
+    } else {
+      console.log("messageContainerRef.current is null");
+    }
+  };
 
-  const handleSignIn = async () => {
-    //const stringifiedSignOut = JSON.stringify(false);
+  const handleSignIn = async (event) => {
+    event.preventDefault();
+
     const formData = new FormData(form.current);
 
     const data = {
@@ -51,49 +42,40 @@ function SignIn() {
       password: formData.get("password"),
     };
 
-    if (data.name == "" || data.password == "") {
-      
-    } else {
-      //console.log("REQUEST DATA", data);
+    if (data.name === "" || data.password === "") {
+      showMessage("warning", messages.errorCredenciales);
+      return;
+    }
+
+    try {
       const response = await signInWs(data.email, data.password);
-      //console.log("RESPONSE DATA", response);
-      //localStorage.setItem('sign-out', data.accessToken);
 
       if (response.success) {
-        // Create account
         const stringifiedAccount = JSON.stringify(data);
-        console.log("DATOS1", data);
-        console.log("DATOS2", stringifiedAccount);
-        //context.setMessageType("");
-        //context.setMessage("Ingrese las credenciales");
         localStorage.setItem("account", stringifiedAccount);
         context.setAccount(data);
         context.setSignOut(false);
 
-        navigate("/");
+        showMessage("success", messages.welcome);
+        setTimeout(() => {
+          navigate("/");
+        }, 1000);
       } else {
-        //context.setMessage(response.errorMessage);
+        showMessage("error", messages.errorLogin);
       }
-      // Redirect
+    } catch (error) {
+      showMessage("error", messages.errorConexion);
     }
   };
 
-  const createAnAccount = () => {
-    // Sign In
-    handleSignIn();
+  const createAnAccount = (event) => {
+    event.preventDefault();
+    handleSignIn(event);
   };
 
   const renderLogIn = () => {
     return (
-      <form ref={form} className="flex flex-col gap-4 w-80">
-        {/* <p>
-          <span className='font-light text-sm'>Email: </span>
-          <span>{parsedAccount?.email}</span>
-        </p>
-        <p>
-          <span className='font-light text-sm'>Password: </span>
-          <span>{parsedAccount?.password}</span>
-        </p> */}
+      <form ref={form} className="flex flex-col gap-4 w-80" onSubmit={handleSignIn}>
         <div className="flex flex-col gap-1">
           <label htmlFor="email" className="font-light text-sm">
             Usuario:
@@ -112,7 +94,7 @@ function SignIn() {
             Contraseña:
           </label>
           <input
-            type="text"
+            type="password"
             id="password"
             name="password"
             defaultValue={parsedAccount?.password}
@@ -120,15 +102,12 @@ function SignIn() {
             className="rounded-lg border border-black placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4"
           />
         </div>
-        <Link to="/">
-          <button
-            className="bg-black disabled:bg-black/40 text-white  w-full rounded-lg py-3 mt-4 mb-2"
-            onClick={() => handleSignIn()}
-            //disabled={!hasUserAnAccount}
-          >
-            Entrar
-          </button>
-        </Link>
+        <button
+          className="bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2"
+          type="submit"
+        >
+          Entrar
+        </button>
         <div className="text-center">
           <a
             className="font-light text-xs underline underline-offset-4"
@@ -140,7 +119,6 @@ function SignIn() {
         <button
           className="border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3"
           onClick={() => setView("create-user-info")}
-          //disabled={hasUserAnAccount}
         >
           Registrarse
         </button>
@@ -150,7 +128,7 @@ function SignIn() {
 
   const renderCreateUserInfo = () => {
     return (
-      <form ref={form} className="flex flex-col gap-4 w-80">
+      <form ref={form} className="flex flex-col gap-4 w-80" onSubmit={createAnAccount}>
         <div className="flex flex-col gap-1">
           <label htmlFor="name" className="font-light text-sm">
             Nombre:
@@ -167,7 +145,7 @@ function SignIn() {
         </div>
         <div className="flex flex-col gap-1">
           <label htmlFor="email" className="font-light text-sm">
-            Usuario
+            Usuario:
           </label>
           <input
             type="text"
@@ -181,10 +159,10 @@ function SignIn() {
         </div>
         <div className="flex flex-col gap-1">
           <label htmlFor="password" className="font-light text-sm">
-            Your password:
+            Contraseña:
           </label>
           <input
-            type="text"
+            type="password"
             id="password"
             name="password"
             defaultValue={parsedAccount?.password}
@@ -193,19 +171,15 @@ function SignIn() {
             placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4"
           />
         </div>
-        <Link to="/">
-          <button
-            className="bg-black text-white w-full rounded-lg py-3"
-            onClick={() => createAnAccount()}
-          >
-            Create
-          </button>
-        </Link>
-        <Link to="/">
-          <button className="border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3 w-full">
-            Cancelar
-          </button>
-        </Link>
+        <button
+          className="bg-black text-white w-full rounded-lg py-3"
+          type="submit"
+        >
+          Create
+        </button>
+        <button className="border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3 w-full">
+          Cancelar
+        </button>
       </form>
     );
   };
@@ -216,20 +190,7 @@ function SignIn() {
   return (
     <Layout>
       <h1 className="font-medium text-xl text-center mb-6 w-80">Bienvenido</h1>
-      {/* {showMessage && (
-        <div
-          class={`fixed top-0 right-0 m-4 mt-20 p-4 ${
-            context.messageType == "Error"
-              ? "bg-red-100 border border-red-400 text-red-700"
-              : "bg-green-100 border border-green-400 text-green-700"
-          } px-4 py-3 relative" role="alert" rounded-lg shadow-lg ${
-            isExiting ? "animate-slideOutToRight" : "animate-slideInFromLeft"
-          }`}
-        >
-          <strong class="font-bold">⚠️</strong>
-          <span class="block sm:inline"> {context.message}</span>
-        </div>
-      )} */}
+      <MessageContainer ref={messageContainerRef} />
       {renderView()}
     </Layout>
   );
---
+++ src/consts/Utilities.tsx
@@ -0,0 +1,9 @@
+export const messages = {
+  welcome: "Bienvenido a la aplicación.",
+  
+  //Errores
+  errorDataLoad: "Error al cargar datos.",
+  errorLogin: "Error al intentar iniciar sesión.",
+  errorCredenciales: "Por favor, complete los campos.",
+  errorConexion: "Error de conexión. Intenta de nuevo más tarde."
+};
